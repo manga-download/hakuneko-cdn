1dfda433c167e5b3cce9cc37eeb7b740f9c23c5a
Ronny Wegener <wegener.ronny@gmail.com>
2019-01-26 23:34:25 +0100

    added support for mangago

---
+++ mangago.js
@@ -0,0 +1,27 @@
+const fs = require( 'fs' );
+const hakuneko = require( 'hakuneko' );
+const connector = hakuneko.mangago;
+
+var pageFrom = ( process.argv.length > 2 ? process.argv[2] : 1 );
+var pageTo = ( process.argv.length > 3 ? process.argv[3] : 9999 );
+var pageFrom = parseInt( pageFrom ) || 1;
+var pageTo = parseInt( pageTo ) || 9999;
+
+/************
+ *** MAIN ***
+ ************/
+
+connector.getMangas( ( error, mangaListWeb ) => {
+    if( !error && mangaListWeb && mangaListWeb.length > 0 ) {
+	// convert mangas into stored structure
+	let mangas = mangaListWeb.map( manga => {
+	    return {
+                id: manga.u,
+                title: manga.t
+            };
+        } );
+        fs.writeFile( './mangago/mangas.json', JSON.stringify( mangas, null, 2 ), 'utf-8', error => {} );
+    } else {
+        console.error( 'Invalid manga list' );
+    }
+}, pageFrom, pageTo );
