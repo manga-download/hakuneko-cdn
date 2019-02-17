4d5b0c5791ca583f33c9a144abc9ccf54d368349
Ronny Wegener <wegener.ronny@gmail.com>
2019-02-17 12:39:24 +0100

    updated sync script

--- _sync.sh
+++ _sync.sh
@@ -5,7 +5,9 @@ git pull
 rm -r -f node_modules
 npm install
 node --max-old-space-size=4096 kissmanga.js
+node --max-old-space-size=4096 kissanime.js
 node --max-old-space-size=4096 mangago.js
+node --max-old-space-size=4096 linewebtoon.js
 git add .
 git commit -m 'autoupdate'
 git push
